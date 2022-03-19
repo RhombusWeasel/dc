a980b34912b7a9392fc88172c9c0ea2fd5537a67
RhombusWeasel <RhombusWeasel@gmail.com>
2022-03-19 18:56:53 +0000

    Basics 0.1.10

---
+++ module/data/generic_fantasy.js
@@ -0,0 +1,41 @@
+let gfd = {
+    ability_points: 5,
+    abilities: [
+        {
+            label: 'Strength',
+            key: 'str',
+            value: 1,
+            max: 5
+        },
+        {
+            label: 'Dexterity',
+            key: 'dex',
+            value: 1,
+            max: 5
+        },
+        {
+            label: 'Constitution',
+            key: 'con',
+            value: 1,
+            max: 5
+        },
+        {
+            label: 'Intelligence',
+            key: 'int',
+            value: 1,
+            max: 5
+        },
+        {
+            label: 'Charisma',
+            key: 'cha',
+            value: 1,
+            max: 5
+        },
+        {
+            label: 'Wisdom',
+            key: 'wis',
+            value: 1,
+            max: 5
+        },
+    ],
+}
--- module/system.js
+++ module/system.js
@@ -6,6 +6,27 @@ import gm_sheet from "./sheets/gm.js"
 
 let SYSTEM_NAME = 'dc'
 
+function register_sheets() {
+    Actors.unregisterSheet("core", ActorSheet);
+    Actors.registerSheet(SYSTEM_NAME, hero_sheet, {makeDefault: true});
+    Actors.registerSheet(SYSTEM_NAME, npc_sheet, {makeDefault: false});
+    Actors.registerSheet(SYSTEM_NAME, enemy_sheet, {makeDefault: false});
+    Actors.registerSheet(SYSTEM_NAME, gm_sheet, {makeDefault: false});
+}
+
+function register_settings() {
+    game.settings.register('deadlands_classic', 'combat_active', {
+        name: 'Combat Active',
+        scope: 'world',     // "world" = sync to db, "client" = local storage 
+        config: false,       // false if you dont want it to show in module config
+        type: Boolean,       // Number, Boolean, String,  
+        default: false,
+        onChange: value => {
+            console.log('Combat Active: ', value);
+        }
+    });
+}
+
 async function preload_handlebars_templates() {
     const template_paths = [
         'systems/dc/templates/tabs/core.hbs',
@@ -66,12 +87,8 @@ function load_handlebars_helpers() {
 Hooks.once("init", function () {
     console.log("Initializing System.");
 
-    Actors.unregisterSheet("core", ActorSheet);
-    Actors.registerSheet(SYSTEM_NAME, hero_sheet, {makeDefault: true});
-    Actors.registerSheet(SYSTEM_NAME, npc_sheet, {makeDefault: false});
-    Actors.registerSheet(SYSTEM_NAME, enemy_sheet, {makeDefault: false});
-    Actors.registerSheet(SYSTEM_NAME, gm_sheet, {makeDefault: false});
-
+    register_sheets();
+    register_settings();
     load_handlebars_helpers();
     preload_handlebars_templates();
 });
