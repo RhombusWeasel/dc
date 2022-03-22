04e6fd8c78ceaee94a3bc8dd812f16ef64ec4efc
RhombusWeasel <RhombusWeasel@gmail.com>
2022-03-22 22:00:44 +0000

    Races 0.2.21

--- module/sheets/gm.js
+++ module/sheets/gm.js
@@ -4,20 +4,20 @@ export default class GMSheet extends HeroSheet {
 
     /** @override */
     static get defaultOptions() {
-      return mergeObject(super.defaultOptions, {
-        classes: ["style_doc"],
-        width: 400,
-        height: 800,
-        tabs: [
-          {navSelector: ".sheet-tabs", contentSelector: ".sheet-body", initial: "system"},
-          {navSelector: ".system-tabs", contentSelector: ".system-body", initial: "races"},
-        ]
-      });
+        return mergeObject(super.defaultOptions, {
+            classes: ["style_doc"],
+            width: 400,
+            height: 800,
+            tabs: [
+            {navSelector: ".sheet-tabs", contentSelector: ".sheet-body", initial: "system"},
+            {navSelector: ".system-tabs", contentSelector: ".system-body", initial: "races"},
+            ]
+        });
     }
   
     /** @override */
     get template() {
-      return `systems/dc/templates/${this.actor.data.type}-sheet.html`;
+        return `systems/dc/templates/${this.actor.data.type}-sheet.html`;
     }
   
     /** @override */
@@ -28,7 +28,17 @@ export default class GMSheet extends HeroSheet {
     }
   
     activateListeners(html) {
+        //Buttons:
+        //System Editor
+        html.find(".race-edit").click(this._on_race_edit.bind(this));
         return super.activateListeners(html);
+    }
+
+    //System Editor Functions:
+    _on_race_edit(ev) {
+        ev.preventDefault()
+        let el = ev.currentTarget;
+        
     }
 
 }
