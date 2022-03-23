dbdd8046c0188c39e2566247a09d012aafffeeab
RhombusWeasel <RhombusWeasel@gmail.com>
2022-03-23 18:13:53 +0000

    Editors 0.1.1

---
+++ module/forms/race-editor.js
@@ -0,0 +1,43 @@
+class RaceEditor extends FormApplication {
+    constructor(race, variant) {
+        super();
+        let tmp = utils.journal.load(game.settings.get('dc', 'system_journal'));
+        this.race = race;
+        this.variant = variant;
+        if (race != 'New Race') {
+            if (variant) {
+                this.race_edits = tmp.races[race].variants[variant];
+            }
+        }else{
+            this.race_edits = utils.system.race_template();
+        }
+    }
+  
+    static get defaultOptions() {
+        return mergeObject(super.defaultOptions, {
+            classes: ['style_doc'],
+            popOut: true,
+            template: `systems/dc/templates/forms/race-editor.html`,
+            id: 'race-editor',
+            title: 'Race Editor',
+        });
+    }
+  
+    getData() {
+        // Return data to the template
+        return {
+            msg: this.exampleOption,
+            color: 'red',
+        };
+    }
+  
+    activateListeners(html) {
+        super.activateListeners(html);
+    }
+  
+    async _updateObject(event, formData) {
+        console.log(formData.exampleInput);
+    }
+}
+  
+window.RaceEditor = RaceEditor;
