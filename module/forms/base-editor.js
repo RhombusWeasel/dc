2a7c23c422a891ee1fb07a3f2c0e584844800255
RhombusWeasel <RhombusWeasel@gmail.com>
2022-03-24 21:59:39 +0000

    Editors 0.2.52

--- module/forms/base-editor.js
+++ module/forms/base-editor.js
@@ -1,9 +1,11 @@
 export default class BaseEditor extends FormApplication {
-    constructor(editor_type) {
+    constructor(editor_type, path) {
         super();
         //console.log('DC : BaseEditor : constructor :', editor_type);
+        this.uuid        = utils.tools.uuid(4, 4, 4, 4);
         this.editor_type = editor_type;
-        this.uuid        = utils.tools.uuid(4, 4, 4, 4)
+        this.root_path   = path;
+        this.path        = `${path}.${this.uuid}`;
     }
   
     static get defaultOptions() {
@@ -49,32 +51,38 @@ export default class BaseEditor extends FormApplication {
     _on_add_line(ev) {
         ev.preventDefault();
         let el = ev.currentTarget;
-        let list = utils.tools.get_path(this.race_edits, el.dataset.path);
+        let list = utils.tools.get_path(this.edits, el.dataset.path);
         list.push('');
-        utils.tools.set_path(this.race_edits, el.dataset.path, list);
+        utils.tools.set_path(this.edits, el.dataset.path, list);
         this.render(true);
     }
 
     _on_toggle_value(ev) {
         ev.preventDefault();
         let el = ev.currentTarget;
-        utils.tools.set_path(this.race_edits, el.dataset.path, !utils.tools.get_path(this.race_edits, el.dataset.path));
+        utils.tools.set_path(this.edits, el.dataset.path, !utils.tools.get_path(this.edits, el.dataset.path));
         this.render(true);
     }
 
     _on_text_change(ev) {
         ev.preventDefault();
         let el = ev.currentTarget;
-        utils.tools.set_path(this.race_edits, el.dataset.path, el.value);
-        console.log(`${this.uuid}-${el.dataset.path}`);
+        utils.tools.set_path(this.edits, el.dataset.path, el.value);
+        if (el.dataset.path == 'label') this.path = `${this.root_path}.${utils.tools.safe_key(el.value)}`;
         document.getElementById(`${this.uuid}-${el.dataset.path}`).innerText = el.value;
     }
 
     _on_int_change(ev) {
         ev.preventDefault();
         let el = ev.currentTarget;
-        utils.tools.set_path(this.race_edits, el.dataset.path, parseInt(el.value));
+        utils.tools.set_path(this.edits, el.dataset.path, parseInt(el.value));
         this.render(true);
+    }
+
+    _on_save_changes(ev) {
+        ev.preventDefault();
+        let el = ev.currentTarget;
+        utils.tools.set_path(utils.template, this.path, this.edits);
     }
 }
   
--- module/forms/race-editor.js
+++ module/forms/race-editor.js
@@ -1,18 +1,19 @@
 import BaseEditor from "./base-editor.js"
 
 class RaceEditor extends BaseEditor {
-    constructor(header, path, race) {
-        super('race');
-        this.game_data   = utils.journal.load(game.settings.get('dc', 'system_journal'));
-        this.header      = header;
-        this.race        = header == 'Bloodline' ? this.game_data[race] : false;
-        this.race_edits  = utils.tools.get_path(this.game_data, path) ? utils.tools.get_path(this.game_data, path) : utils.system.race_template();
+    constructor(header, path, bloodline) {
+        super('race', path);
+        this.game_data = utils.journal.load(game.settings.get('dc', 'system_journal'));
+        this.header    = header;
+        this.race      = header == 'Bloodline' ? this.game_data[race] : false;
+        this.bloodline = bloodline;
+        this.edits     = utils.tools.get_path(this.game_data, path) ? utils.tools.get_path(this.game_data, path) : utils.system.race_template();
     }
   
     getData() {
-        let data = super.getData();
+        let data   = super.getData();
         data.title = this.header;
-        data.race  = this.race_edits;
+        data.race  = this.edits;
         data.stats = utils.template.entity_template.stats;
         return data;
     }
