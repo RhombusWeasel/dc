1c2451a600fc431748e975da551ef76aa2974ab2
RhombusWeasel <RhombusWeasel@gmail.com>
2022-03-25 16:18:08 +0000

    Editors 0.3.39

--- module/sheets/hero.js
+++ module/sheets/hero.js
@@ -31,7 +31,7 @@ export default class HeroSheet extends ActorSheet {
     /** @override */
     getData() {
         const data = super.getData();
-        data.stats = utils.system.new.stats();
+        data.stats = utils.system.new.stats().stats;
         if (this.actor.data.data.race == 'None') data.races = utils.game_data.races
         return data;
     }
