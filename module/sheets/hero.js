66fcd2a6437687319c9ce6b9afe013cd30f8a7b4
RhombusWeasel <RhombusWeasel@gmail.com>
2022-03-25 18:33:21 +0000

    Editors 0.3.44

--- module/sheets/hero.js
+++ module/sheets/hero.js
@@ -1,53 +1,58 @@
 export default class HeroSheet extends ActorSheet {
 
-    /** @override */
-    static get defaultOptions() {
-      let races = utils.game_data.races;
-      let tabs = [
-        {navSelector: ".sheet-tabs", contentSelector: ".sheet-body", initial: "core"},
-        {navSelector: ".race-tabs", contentSelector: ".race-body", initial: "human"},
-      ];
-      for (const key in races) {
-        if (Object.hasOwnProperty.call(races, key)) {
-          const race = races[key];
-          if (race?.variants) {
-            tabs.push({ navSelector: `.${key}-tabs`, contentSelector: `.${key}-body`, initial: `${Object.keys(race.variants)[0]}`});
-          }
+  /** @override */
+  static get defaultOptions() {
+    let races = utils.game_data.races;
+    let tabs = [
+      {navSelector: ".sheet-tabs", contentSelector: ".sheet-body", initial: "core"},
+      {navSelector: ".race-tabs", contentSelector: ".race-body", initial: "human"},
+    ];
+    for (const key in races) {
+      if (Object.hasOwnProperty.call(races, key)) {
+        const race = races[key];
+        if (race?.variants) {
+          tabs.push({ navSelector: `.${key}-tabs`, contentSelector: `.${key}-body`, initial: `${Object.keys(race.variants)[0]}`});
         }
       }
-      return mergeObject(super.defaultOptions, {
-        classes: ["style_doc"],
-        width: 400,
-        height: 800,
-        tabs: tabs
-      });
     }
-  
-    /** @override */
-    get template() {
-      return `systems/dc/templates/${this.actor.data.type}-sheet.html`;
-    }
-  
-    /** @override */
-    getData() {
-        const data = super.getData();
-        data.char  = utils.system.new.entity();
-        data.stats = data.char.stats;
-        if (this.actor.data.data.race == 'None') data.races = utils.game_data.races
-        console.log('DC : hero.js :', this);
-        return data;
-    }
-  
-    activateListeners(html) {
-        //Example Button Registry
-        html.find(".test-button").click(this._on_test_button.bind(this));
-  
-        html.find(".class-select").change(this._on_test_dropdown.bind(this));
-        return super.activateListeners(html);
-    }
+    return mergeObject(super.defaultOptions, {
+      classes: ["style_doc"],
+      width: 400,
+      height: 800,
+      tabs: tabs
+    });
+  }
 
-    _on_test_button(ev) {}
+  /** @override */
+  get template() {
+    return `systems/dc/templates/${this.actor.data.type}-sheet.html`;
+  }
 
-    _on_test_dropdown(ev) {}
+  /** @override */
+  getData() {
+    const data  = super.getData();
+    if (this.actor.data.data.race == 'None') {
+      data.race = this._tabs.filter((r) => {return r._contentSelector = '.race-body'})[0].active;
+    }else{
+      data.race = this.actor.data.data.race;
+    }
+    data.char   = utils.system.new.entity();
+    data.stats  = data.char.stats;
+    if (this.actor.data.data.race == 'None') data.races = utils.game_data.races
+    console.log('DC : hero.js :', this);
+    return data;
+  }
+
+  activateListeners(html) {
+    //Example Button Registry
+    html.find(".test-button").click(this._on_test_button.bind(this));
+
+    html.find(".class-select").change(this._on_test_dropdown.bind(this));
+    return super.activateListeners(html);
+  }
+
+  _on_test_button(ev) {}
+
+  _on_test_dropdown(ev) {}
 
 }
