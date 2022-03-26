012036f6f4acbd7d1b4adc6b9485a8eb93c007e7
RhombusWeasel <RhombusWeasel@gmail.com>
2022-03-26 17:46:41 +0000

    Editors 0.4.49

--- module/forms/base-editor.js
+++ module/forms/base-editor.js
@@ -111,6 +111,7 @@ export default class BaseEditor extends FormApplication {
         ev.preventDefault();
         let el = ev.currentTarget;
         this.mod_index = parseInt(el.value);
+        console.log(this.mod_index);
         this.getData();
     }
 
